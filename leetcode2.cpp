86b3242050455c96a6366db9999887da5a6c9514
Mayukh <mayukhranaghat2012@gmail.com>
2020-06-21 11:53:37 +0530

    priority queue customization added

---
+++ leetcode2.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+private:
+struct Comp{
+    Comp(){}
+    ~Comp(){}
+    bool operator()(const vector<int>& a, const vector<int>& b){
+        return (a[0]*a[0] + a[1]*a[1]) > (b[0]*b[0] + b[1]*b[1]);
+    }
+};
+public:
+vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+    if(points.size() == 0) return {};
+    priority_queue<vector<int>, vector<vector<int>>, Comp> pq;
+    vector<vector<int>> res;
+    for(auto x:points){
+        pq.push(x);
+    }
+    while(K--){
+        res.push_back(pq.top());
+        pq.pop();
+    }
+    return res;
+}
+};
+int main(){
+    vector<vector<int>> arr = {{1,3},{-2,2}};
+    int N = 1;
+    Solution s;
+    vector<vector<int>> ans =  s.kClosest(arr,N);
+    cout<<"[";
+    for(int i=0;i<ans.size();i++){
+        cout<<"[";
+        cout<<ans[i][0]<<","<<ans[i][1];
+        cout<<"]";
+    }
+    cout<<"]"<<endl;
+    return 0;
+}
