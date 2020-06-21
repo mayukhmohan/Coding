e1ff39318907b35c61381493363a758e644ddea0
Mayukh <mayukhranaghat2012@gmail.com>
2020-06-21 11:49:15 +0530

    rabin_karp_binary_search

--- leetcode1.cpp
+++ leetcode1.cpp
@@ -1,77 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution{
-private:
-struct Comp{
-    Comp(){}
-    ~Comp(){}
-    bool operator()(const vector<int>& a, const vector<int>& b){
-        return (a[0]*a[0] + a[1]*a[1]) > (b[0]*b[0] + b[1]*b[1]);
+class Solution {
+    #define d 256 
+    long int q  = 1e7 + 7;
+    vector<long int> power;
+    string rabin_karp(string t, long int l){
+        if(l == 0) return "";
+        long int hash = 0;
+        long int N = t.length();
+        unordered_map<long int, vector<long int>> m; //map index of string for same hash value
+        for(long int i = 0; i < l; i++){
+            hash = (hash * d + (t[i] - 'a'))%q;
+            m[hash].push_back(0);
+        }
+        for(long int i=0;i<(N - l);i++){
+            hash = ((d * (hash - (t[i] - 'a') * power[l-1]) + (t[i+l] - 'a'))%q + q)%q;
+            if(m.find(hash) == m.end()) m[hash].push_back(i+1);
+            else{
+                for(long int index: m[hash]) {
+                    if(t.substr(index, l) == t.substr(i+1, l))  return t.substr(index, l);
+                }
+                m[hash].push_back(i+1);
+            }
+        }
+        return "";
     }
-};
 public:
-vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-    if(points.size() == 0) return {};
-    priority_queue<vector<int>, vector<vector<int>>, Comp> pq;
-    vector<vector<int>> res;
-    for(auto x:points){
-        pq.push(x);
+    string longestDupSubstring(string S) {
+        long int l = 0, N = S.length(), r = N - 1;
+        for(long int i=0;i<N;i++){
+            power.push_back(0);
+            power[i] = (i==0) ? 1 : (d * power[i-1])%q;
+        }
+        string res = "";
+        while(l<=r){
+            long int m = l + (r - l)/2;
+            string ans = rabin_karp(S,m);
+            if(ans.length()>res.length()){
+                res = ans;
+                l = m + 1;
+            }
+            else r = m - 1; 
+        }
+        return res;
     }
-    while(K--){
-        res.push_back(pq.top());
-        pq.pop();
-    }
-    return res;
-}
 };
-/*class Solution {
-    private:
-    struct Comp {
-        Comp() {}
-        ~Comp() {}
-        bool operator()(const vector<int>& a, const vector<int>& b) {
-            return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
-        }
-    };    
-
-    public:
-        vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-
-            if (points.size() == 0)
-                return {};
-
-            priority_queue< vector<int>, vector<vector<int>>, Comp> pq;
-            
-            for (auto kv : points) {
-                pq.push(kv);
-            
-                if(pq.size() > K) pq.pop();
-            }
-            
-            vector<vector<int>> res;
-            
-            while (!pq.empty()) {
-            
-                res.push_back(pq.top());
-
-                pq.pop();
-            }
-            
-            return res;
-
-        }
-    };*/
 int main(){
-    vector<vector<int>> arr = {{1,3},{-2,2}};
-    int N = 1;
-    Solution s;
-    vector<vector<int>> ans =  s.kClosest(arr,N);
-    cout<<"[";
-    for(int i=0;i<ans.size();i++){
-        cout<<"[";
-        cout<<ans[i][0]<<","<<ans[i][1];
-        cout<<"]";
+    string S = "geeksforgeeks";
+    Solution* obj = new Solution();
+    int T = 1;
+    while(T--){
+        cout<<obj->longestDupSubstring(S)<<endl;
     }
-    cout<<"]"<<endl;
     return 0;
 }
